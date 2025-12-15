3f8bd004a069c3d948f42cd3a50253236456f456
Pujitha Reddy Kethireddy <pujithareddy1357@gmail.com>
2025-12-15 07:49:01 +0000

    added output zero

---
+++ Getting_Started/Output_Zero.v
@@ -0,0 +1,7 @@
+module top_module(
+    output zero
+);// Module body starts after semicolon
+// For Quartus synthesis, not assigning a value to a signal usually results in 0. 
+// Good practice to assign a value to a signal explicitly
+	assign zero = 1'b0;
+endmodule
